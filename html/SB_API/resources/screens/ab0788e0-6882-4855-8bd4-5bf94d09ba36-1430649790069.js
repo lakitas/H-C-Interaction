2eae1e2cc63d0fb6f9a4f45c36010630794540e0
lakitas <kittaslabros@gmail.com>
2015-05-03 13:48:39 +0300

    Adding the html files responsible for preview

---
+++ html/SB_API/resources/screens/ab0788e0-6882-4855-8bd4-5bf94d09ba36-1430649790069.js
@@ -0,0 +1,179 @@
+jQuery("#simulation")
+  .on("click", ".s-ab0788e0-6882-4855-8bd4-5bf94d09ba36 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Label_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/94648e5c-3eff-4b77-ba19-7c0727bbe09d"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/068c35a3-a3eb-4a0e-b3fd-d826c3f09439"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9df1bad1-52b7-438f-8038-9e522bd84900"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/1aa45bc7-5b70-41f9-811b-eb233d15fd7e"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e92676f7-5d34-410e-b7ac-dd77a7c63387"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e92676f7-5d34-410e-b7ac-dd77a7c63387"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/94648e5c-3eff-4b77-ba19-7c0727bbe09d"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_9")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/068c35a3-a3eb-4a0e-b3fd-d826c3f09439"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_10")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9df1bad1-52b7-438f-8038-9e522bd84900"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
