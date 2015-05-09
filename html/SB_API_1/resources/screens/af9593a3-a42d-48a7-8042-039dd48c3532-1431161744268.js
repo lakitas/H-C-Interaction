fb70318030831918601e3d45c995778b903f3145
lakitas <kittaslabros@gmail.com>
2015-05-09 11:56:38 +0300

    Updating the html folder

---
+++ html/SB_API_1/resources/screens/af9593a3-a42d-48a7-8042-039dd48c3532-1431161744268.js
@@ -0,0 +1,103 @@
+jQuery("#simulation")
+  .on("click", ".s-af9593a3-a42d-48a7-8042-039dd48c3532 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e92676f7-5d34-410e-b7ac-dd77a7c63387"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e92676f7-5d34-410e-b7ac-dd77a7c63387"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/068c35a3-a3eb-4a0e-b3fd-d826c3f09439"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9df1bad1-52b7-438f-8038-9e522bd84900"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/ab0788e0-6882-4855-8bd4-5bf94d09ba36"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
---
+++ html/SB_API_1/resources/screens/e92676f7-5d34-410e-b7ac-dd77a7c63387-1431161744268.js
@@ -0,0 +1,103 @@
+jQuery("#simulation")
+  .on("click", ".s-e92676f7-5d34-410e-b7ac-dd77a7c63387 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e92676f7-5d34-410e-b7ac-dd77a7c63387"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/94648e5c-3eff-4b77-ba19-7c0727bbe09d"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/068c35a3-a3eb-4a0e-b3fd-d826c3f09439"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/9df1bad1-52b7-438f-8038-9e522bd84900"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Label_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/1aa45bc7-5b70-41f9-811b-eb233d15fd7e"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
